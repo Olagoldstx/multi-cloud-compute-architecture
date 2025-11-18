0d05157ec76c817aeb77713eeb9c584e36144f7c
Olagoldstx <107962449+Olagoldstx@users.noreply.github.com>
2025-11-17 19:22:58 -0800

    Update main.tf

--- terraform/modules/terraform/stacks/multi-cloud-deployment/main.tf
+++ terraform/modules/terraform/stacks/multi-cloud-deployment/main.tf
@@ -87,6 +87,17 @@ resource "aws_kms_key" "ec2" {
 }
 
 ###############################################
+# AWS ZERO TRUST NETWORK MODULE
+###############################################
+
+module "aws_zero_trust" {
+  source          = "../../modules/aws-network"
+  vpc_id          = aws_vpc.main.id
+  private_subnets = [aws_subnet.private.id]
+  region          = var.aws_region
+}
+
+###############################################
 # AWS EC2 MODULE
 ###############################################
 
